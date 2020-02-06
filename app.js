82b176c456362496541da60083ef0cea9e61c10f
Daisuke Yamamura <mucho613@gmail.com>
2020-02-07 02:47:17 +0900

    リファクタリング

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const app = express();
-const path = require('path');
 
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 80;
 
-// ファイル保存用
-const fs = require('fs');
-
-const { createCanvas, loadImage } = require('canvas')
+const { createCanvas } = require('canvas')
 
 let layer = createCanvas(2000, 2000);
 let ctx = layer.getContext('2d');
@@ -18,36 +14,33 @@ http.listen(PORT, () => {
   console.log('Running at Port ' + PORT);
 });
 
-// 静的ファイルのルーティング
-app.use(express.static(path.join(__dirname, 'public')));
-
 // その他のリクエストに対する404エラー
 app.use((req, res) => {
   res.sendStatus(404);
 });
 
-io.on('connection', function (socket) {
+io.on('connection', socket => {
   // 新しく入ったユーザーのために、描いてたやつを送信する
   let base64 = layer.toDataURL();
   socket.emit('init', { imageData: base64 });
   console.log("New connection established. Image transmitted.");
 
   // クライアントからメッセージ受信
-  socket.on('clear send', function () {
+  socket.on('clear send', () => {
     socket.broadcast.emit('clear user');
     // サーバー側でも全消しする
     allClear();
   });
 
   // クライアントからメッセージ受信
-  socket.on('server send', function (msg) {
+  socket.on('server send', msg => {
     socket.broadcast.emit('send user', msg);
     // サーバー側でも描画する
     drawCore(msg.x1, msg.y1, msg.x2, msg.y2, msg.color, msg.thickness)
   });
 
   // 切断
-  socket.on('disconnect', function () {
+  socket.on('disconnect', () => {
     // io.sockets.emit('user disconnected');
   });
 });
@@ -70,4 +63,3 @@ function allClear() {
   ctx.fillStyle = "#f5f5f5";
   ctx.fillRect(0, 0, 2000, 2000);
 }
-
